7681a98e4c58168a0d6fd96c0499285a53c2ce4a
YoussefShaker22 <youssefaymanshaker@outlook.com>
2025-07-20 13:30:51 +0300

    Set up SqlServer

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f85fec4995d1d614ee43f7189adac7844f3ad2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af16b8e053e28dbc84992a5ce069f4ee8ce277ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db1e39cea6860deabd17e3affb5e7fc9104c9fa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af16b8e053e28dbc84992a5ce069f4ee8ce277ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
